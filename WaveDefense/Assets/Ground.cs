55c7c0d57983c785b354c7e7cebf09f6bf04c3ac
Michl Steglich <mrgreaka@gmail.com>
2017-01-20 21:52:23 +0100

    base stuff

---
+++ WaveDefense/Assets/Ground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+    Mesh Plane;
+
+	// Use this for initialization
+	void Start () {
+        Plane = new Mesh();
+        Plane.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
