936fe2abd8878a7918843657ac353f38c3b9e3a3
Jannes <Jannes@DESKTOP-006OCGK>
2017-01-21 01:02:24 +0100

    Tower und ein bisschen Startmenuu

--- WaveDefense/Assets/Enem.cs
+++ WaveDefense/Assets/Enem.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour {
-
-    Mesh Plane;
+public class Enem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Plane = new Mesh();
-        Plane.
+		
 	}
 	
 	// Update is called once per frame
---
+++ WaveDefense/Assets/Tower_Angriff.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Tower_Angriff : MonoBehaviour {
+
+    public int a_count = 0;
+    private double Enemey;
+        //Math.Sqrt(Math.Abs(Enemey.Transform.Position.x-transform.position.X)^2+ Math.Abs(Enemey.Transform.Position.x - this.transform.position.X)^2);
+    public Collider2D inRange;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// FixedUpdate is called once per tick
+	void FixedUpdate () {
+        a_count++;
+        if (a_count > 3)
+            attack();
+	}
+
+    public void attack()
+    {
+        if (inRange.isTrigger)
+        {
+            //Attack
+            //Enemey.life--;
+            a_count = 0;
+        }
+    }
+}
