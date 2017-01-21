b783b18ad916c03d1fe88870753428137eabe2b0
Jannes <Jannes@DESKTOP-006OCGK>
2017-01-21 18:52:34 +0100

    camera

---
+++ WaveDefense/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var position = gameObject.transform.position;
+        if (Input.GetKeyDown("a"))
+            position += new Vector3(-1, 0, 0);
+        if (Input.GetKeyDown("s"))
+            position += new Vector3(0, -1, 0);
+        if (Input.GetKeyDown("d"))
+            position += new Vector3(1, 0, 0);
+        if (Input.GetKeyDown("w"))
+            position += new Vector3(0, 1, 0);
+
+        gameObject.transform.position = position;
+    }
+}
