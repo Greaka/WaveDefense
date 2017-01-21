438cabd4e8f4c3c7febd00489014797fd96dcc1d
Jannes <Jannes@DESKTOP-006OCGK>
2017-01-21 21:07:21 +0100

    da

---
+++ WaveDefense/Assets/Scripts/MapInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MapInfo : MonoBehaviour {
+
+    private static PlayerStats _instance;
+
+    public static int PlayerHealth = 100;
+
+    public static int PlayerWealth = 0;
+
+    public static PlayerStats Instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance;
+            var CharacterLoadout = new GameObject("PlayerStats");
+            _instance = CharacterLoadout.AddComponent<PlayerStats>();
+            return _instance;
+        }
+        private set { }
+    }
+
+    public void OnEnemyDeath(Enemy instance)
+    {
+        if (instance.Health > 0)
+            PlayerHealth -= instance.Damage;
+        else
+            PlayerWealth += instance.Bounty;
+    }
+
+    public void Awake()
+    {
+        if (gameObject.gameObject == null)
+            DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (PlayerHealth <= 0)
+        {
+            Console.WriteLine("GAMEOVER");
+            SceneManager.LoadScene("Lost");
+        }
+
+    }
+}
