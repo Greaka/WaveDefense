86af0becbc6856ae75ad7d89dbe6b6f844fa592a
Jannes <Jannes@DESKTOP-006OCGK>
2017-01-21 08:42:44 +0100

    Titelbildschirm -> Level-Script (Levelname fehlt)

--- WaveDefense/Assets/Startscreen/StartNewGame.cs
+++ WaveDefense/Assets/Startscreen/StartNewGame.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class StartNewGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //StartGameWhenTitlePressed
+    private void OnMouseDown()
+    {
+        Application.LoadLevel("");
+    }
 }
